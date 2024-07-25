ffc6f00c6ef79fbc7529f5a1bd62ae98000eb7bc
Gabriel Tedja <gabetedja3@gmail.com>
2024-07-25 09:02:48 -0700

    Map and libraries added, still need to work on getting user location and scaling

--- CatApp/AddCatView.swift
+++ CatApp/AddCatView.swift
@@ -7,12 +7,16 @@
 
 import SwiftUI
 import PhotosUI //import necessary for photo uploads
+import MapKit
+import CoreLocation //necessary libraries later
+import CoreLocationUI
 
 struct AddCatView: View {
     @State var label: String = "" //you need to declare these variables in the struct
     @State var breed: String = ""
     @State private var catItem: PhotosPickerItem? //cat image stuff
     @State private var catImage: Image?
+
     var body: some View {
         ZStack {
             Background()
@@ -24,7 +28,10 @@ struct AddCatView: View {
                     Text("Breed")
                 }
                 PhotosPicker("Upload image of cat", selection: $catItem, matching: .images)
+                Map()
             }
+            
+            
         }
     }
 }
