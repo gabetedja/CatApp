8f862dcd54b1f07b6420ee576e4e3b613ed4a033
Atharva <atharva.nv@gmail.com>
2024-07-26 06:30:19 -0700

    minor UI tweaks, map centering on user is complete, refactored the code a bit

--- CatApp/AddCatView.swift
+++ CatApp/AddCatView.swift
@@ -16,42 +16,88 @@ struct AddCatView: View {
     @State var breed: String = ""
     @State private var catItem: PhotosPickerItem? //cat image stuff
     @State private var catImage: Image?
-
+    
     var body: some View {
         NavigationView {
             ZStack { // root container
                 Background()
                 VStack (spacing: 0) { // all content goes inside here
+                    
                     Text("Add A New Cat Sighting")
                         .padding(.bottom, -20) // jank padding hack, but i had no idea what was adding the weird space.
-                    Form {
-                        Section() {
-                            TextField(text:$label) {
-                                Text("Cat Nickname")
-                            }
-                            TextField(text:$breed) {
-                                Text("Breed")
-                            }
-                            PhotosPicker("Upload Cat Image", selection: $catItem, matching: .images)
-                        }
-                    }
-                    .frame(height: 195)
-                    .scrollContentBackground(.hidden) // hides the default form background
+                    
+                    AddCatForm(label: $label, breed: $breed, catItem: $catItem)
+                        .frame(height: 195)
+                        .scrollContentBackground(.hidden) // hides the default form background
+                    
                     Text("Where did you see this cat?")
-                        .padding(.bottom, 10)
-                    Map()
+                        .padding(.bottom, 15)
+                    
+                    AddCatMapView()
                 }
+
             }
             .navigationBarHidden(true)
             // ditching the idea of a title bar. too hard to make it not ugly. keeping this here in case we want it back.
             //.navigationBarTitle("Add A Cat Sighting")
             //.navigationBarTitleDisplayMode(.inline) // do we want this?
             
-            // next 2 lines took me way too long to find how to do holy shit. controls back section's color
+            // next 2 lines took me way too long to find how to do holy shit. these control the toolbar's color
             .toolbarBackground(.visible, for: .navigationBar)
             .toolbarBackground(bgColor, for: .navigationBar)
 
         }
+    }
+}
+
+
+
+// Form to handle user input, used bindings to relay the change back to the parent view, and returns a form view
+struct AddCatForm: View {
+    
+    // need to work out if this binding logic makes sense.. not sure fully
+    @Binding var label: String
+    @Binding var breed: String
+    @Binding var catItem: PhotosPickerItem?
+    
+    var body: some View {
+        Form {
+            Section() {
+                TextField(text: $label) {
+                    Text("Cat Nickname")
+                }
+                TextField(text: $breed) {
+                    Text("Breed")
+                }
+                PhotosPicker("Upload Cat Picture", selection: $catItem, matching: .images)
+            }
+        }
+    }
+}
+
+// Handles map logic and returns the map as a view
+struct AddCatMapView: View {
+    
+    @State private var cameraPosition: MapCameraPosition = .userLocation(fallback: .automatic) // not sure exactly what .automatic does.. need to research
+    
+    var body: some View {
+        
+        Map(position: $cameraPosition) {
+            UserAnnotation() // show user location blue dot
+        }
+        .mapControls {
+            MapUserLocationButton() // add location centering (arrow) button
+            MapPitchToggle() // 2d and 3d
+        }
+        .onAppear {
+            CLLocationManager().requestWhenInUseAuthorization() // ask for location permission
+        }
+        
+        // MAP todo list:
+        // initially, center map on user location <DONE>
+        // allow user to tap on map and add marker
+        // then, need to store that data in an object somewhere,
+        // using swiftdata.
     }
 }
 
