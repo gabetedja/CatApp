33a2d068547801e00411879d526425a8197da332
Gabriel Tedja <gabetedja3@gmail.com>
2024-07-28 15:02:45 -0700

    Added some rudimentary UI for the CatDataView, todo: add image and map functionality, and make the whole thing look less ugly

--- CatApp/CatDataView.swift
+++ CatApp/CatDataView.swift
@@ -4,19 +4,43 @@
 //
 //  Created by Atharva on 7/24/24.
 //
-
 import SwiftUI
+import SwiftData
+import PhotosUI
 
 struct CatDataView: View {
+    //reference to database
+    @Environment(\.modelContext) private var context
+    @Query var cats: [Cat] //ensures a loop iterates thru here
+    
+    
+    
+    
     var body: some View {
-        ZStack {
-            Background()
-            
-            Text("cat data stuff here")
+ 
+        List {
+                ForEach(cats, id:\.self) { cat in
+                    VStack {
+                        HStack {
+                            Text("Name")
+                            Text("Breed")
+                        }
+                    }
+                    HStack {
+                        Text(cat.label)
+                        Text(cat.breed)
+                            .background(Rectangle().fill(Color.white).shadow(radius: 3))
+
+                    }
+                }
+                .scrollContentBackground(.hidden)
+            }
+            .navigationTitle("List of Cats")
         }
     }
-}
+
 
 #Preview {
     CatDataView()
+        .modelContainer(for: Cat.self) //container that stores cat objects
 }
