c13ee2791fe09a4518828dbc635a8b090db87fc0
Atharva <atharva.nv@gmail.com>
2024-07-27 18:29:09 -0700

    got basic cats loading on the big mapview, still need to add image integration

--- CatApp/MapView.swift
+++ CatApp/MapView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import MapKit //MapKit obviously necessary
+import MapKit
 import SwiftData
 
 struct MapView: View {
@@ -23,13 +23,41 @@ struct MapView: View {
                         print("\(cat.label) is \(cat.breed) breed. They were seen at (\(cat.latitude),\(cat.longitude))")
                     }
                 }) {
-                    Text("TEST: Load cat data")
+                    Text("TEST (delete this later): Load cat data")
                 }
+                MapSection()
             }
+        }
+    }
+}
+
+// Handles map logic and returns the map as a view
+struct MapSection: View {
+ 
+    // Used to get user's current position.. unsure what fallback does. Redraws the map every time user moves.
+    @State private var cameraPosition: MapCameraPosition = .userLocation(fallback: .automatic)
+    
+    // Create a usable reference to the database.
+    @Environment(\.modelContext) private var context
+    
+    // Querying the database happens through here (eg. for loop)
+    @Query var cats: [Cat]
+        
+    var body: some View {
+        Map(position: $cameraPosition) {
+            UserAnnotation() // show user location blue dot
+            ForEach(cats) { cat in
+                Marker("\(cat.label)", coordinate: CLLocationCoordinate2D(latitude: cat.latitude, longitude: cat.longitude))
+            }
+        }
+        .onAppear {
+            CLLocationManager().requestWhenInUseAuthorization() // ask for location permission
         }
     }
 }
 
 #Preview {
     MapView()
+        .modelContainer(for: Cat.self)
 }
+
